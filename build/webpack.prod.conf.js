b0f4ab54a77562c7aedf65b73bc07a13a086bb0b
Pontus Aurdal <pontus.edvard@gmail.com>
2018-04-24 14:13:26 +0200

    Add moment to bus depts

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -14,7 +14,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const env = require('../config/prod.env')
 
 const webpackConfig = merge(baseWebpackConfig, {
-  mode: 'production',
+  //mode: 'production',
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
